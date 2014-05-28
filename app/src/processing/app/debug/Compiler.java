28b7a542bb96486dca57499fc5fd3731f475b11f
Gerson Fernando Budke <nandojve@gmail.com>
2014-05-28 19:24:07 -0300

    Add HAL_<MCU> option and -std=gnu<11> to gcc command line
    Add options -std=??? and -DHAL<MCU> to gnu compiler. Gcc will compile
    with -std=gnu11 and g++ with -std=gnu++11. The option HAL_<MCU> enable
    Hardware Abstraction Layer to be selected in compile time.

--- app/src/processing/app/debug/Compiler.java
+++ app/src/processing/app/debug/Compiler.java
@@ -637,6 +637,7 @@ public class Compiler implements MessageConsumer {
 
     List baseCommandCompiler = new ArrayList(Arrays.asList(new String[] {
       avrBasePath + "avr-gcc",
+      "-std=gnu11",
       "-c", // compile, don't link
       "-g", // include debugging info (so errors include line numbers)
       "-Os", // optimize for size
@@ -644,6 +645,7 @@ public class Compiler implements MessageConsumer {
       "-ffunction-sections", // place each function in its own section
       "-fdata-sections",
       "-mmcu=" + boardPreferences.get("build.mcu"),
+      "-DHAL_" + boardPreferences.get("build.mcu").toUpperCase(),
       "-DF_CPU=" + boardPreferences.get("build.f_cpu"),
       "-MMD", // output dependancy info
       "-DUSB_VID=" + boardPreferences.get("build.vid"),
@@ -669,6 +671,7 @@ public class Compiler implements MessageConsumer {
     
     List baseCommandCompilerCPP = new ArrayList(Arrays.asList(new String[] {
       avrBasePath + "avr-g++",
+      "-std=gnu++11",
       "-c", // compile, don't link
       "-g", // include debugging info (so errors include line numbers)
       "-Os", // optimize for size
@@ -677,6 +680,7 @@ public class Compiler implements MessageConsumer {
       "-ffunction-sections", // place each function in its own section
       "-fdata-sections",
       "-mmcu=" + boardPreferences.get("build.mcu"),
+      "-DHAL_" + boardPreferences.get("build.mcu").toUpperCase(),
       "-DF_CPU=" + boardPreferences.get("build.f_cpu"),
       "-MMD", // output dependancy info
       "-DUSB_VID=" + boardPreferences.get("build.vid"),
